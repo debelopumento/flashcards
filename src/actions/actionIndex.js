de93dea90d873725393f12f7c4e7fb9e51cbead5
debelopumento <imdiye@gmail.com>
2017-04-25 21:36:05 -0700

    test host url

--- src/actions/actionIndex.js
+++ src/actions/actionIndex.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import store from '../store';
 
+/*
 const host = process.env.NODE_ENV === 'production'
     ? window.location.href
     : 'http://localhost:8080/';
+*/
+const host = 'https://virtual-flashcards.herokuapp.com/';
 
 export const updateFacebookId = facebookId => ({
     type: 'UPDATE_FACEBOOKID',
