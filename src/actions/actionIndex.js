f6b212d41a5faf8722469f781ce27835553be457
debelopumento <imdiye@gmail.com>
2017-05-09 17:55:27 -0700

    updated production url

--- src/actions/actionIndex.js
+++ src/actions/actionIndex.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "../store";
 
 const host = process.env.NODE_ENV === "production"
-    ? window.location.href
+    ? "/"
     : "http://localhost:8080/";
 
 export const updateFacebookId = facebookId => ({
